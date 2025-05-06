f7765dc60a30ff8c8dc6bd8148ca502851fce520
WolfEYc <isaacwolfsite@gmail.com>
2025-05-05 22:44:08 -0500

    use comptime to generate all of the cfuncs at compile time like a chad

--- src/root.zig
+++ src/root.zig
@@ -3,13 +3,54 @@ const testing = std.testing;
 const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 
-// pub export fn add(a: i32, b: i32) i32 {
-//     return a + b;
-// }
+pub const Apply_Mode = enum {
+    Strict,
+    Grow,
+};
+pub const Simd_Op = enum {
+    Add,
+    Sub,
+    Div,
+    Mul,
+    Mod,
+    Min,
+    Max,
+    Log,
+    Pow,
+    // LShift,
+    // RShift,
+    // SLShift,
+    And,
+    Or,
+    Xor,
+};
 
-// test "basic add functionality" {
-//     try testing.expect(add(3, 7) == 10);
-// }
+const simd_float_ops = [_]Simd_Op{
+    .Log,
+    .Pow,
+};
+
+const number_types = [_]type{
+    u8,
+    u16,
+    u32,
+    u64,
+    i8,
+    i16,
+    i32,
+    i64,
+    f32,
+    f64,
+};
+
+const float_types = [_]type{
+    f32, f64,
+};
+
+pub const Operand_Variant = enum {
+    Vector,
+    Number,
+};
 
 fn print_vek(comptime T: type, v: Stream(T)) void {
     const iter = stream_iter(T, v);
@@ -30,9 +71,9 @@ fn print_float_slice(comptime T: type, s: []T) void {
     std.debug.print("\n", .{});
 }
 
-pub const TARGET_SIMD_SIZE: comptime_int = 64; // AVX512
+pub const TARGET_SIMD_BITS: comptime_int = 512; // AVX512
 pub fn lanes(comptime T: type) comptime_int {
-    return TARGET_SIMD_SIZE / @sizeOf(T);
+    return TARGET_SIMD_BITS / 8 / @sizeOf(T);
 }
 
 pub fn Vek(comptime T: type) type {
@@ -118,7 +159,7 @@ pub export fn make_i64(s: *State, stream_len: usize, num_streams: usize) Result(
     return resultify(Streams(i64), make(i64, s.allocator, stream_len, num_streams));
 }
 
-pub fn slice_to_stream(comptime T: type, streams: *Streams(T), slice: []T, start_idx: usize) Stream(T) {
+pub fn to_stream(comptime T: type, streams: *Streams(T), slice: []T, start_idx: usize) Stream(T) {
     var stream = new_stream(T, streams);
     set_stream(T, &stream, slice, start_idx);
     return stream;
@@ -147,10 +188,6 @@ pub fn set_stream(comptime T: type, stream: *Stream(T), slice: []T, start_idx: u
     @memcpy(stream_ptr + start_idx, slice);
 }
 
-pub const Operand_Variant = enum {
-    Vector,
-    Number,
-};
 pub fn Operand(comptime T: type, comptime variant: Operand_Variant) type {
     return switch (variant) {
         .Vector => Stream(T),
@@ -158,43 +195,26 @@ pub fn Operand(comptime T: type, comptime variant: Operand_Variant) type {
     };
 }
 
-pub const Apply_Mode = enum {
-    Strict,
-    Grow,
-};
-pub const Simd_Op = enum {
-    Add,
-    Sub,
-    Div,
-    Mul,
-    Mod,
-    Min,
-    Max,
-    Log,
-    Pow,
-    LShift,
-    RShift,
-    SLShift,
-    And,
-    Or,
-    Xor,
-};
-
 fn idx_vek(comptime T: type) @Vector(lanes(T), usize) {
-    var arr: [lanes(T)]usize = undefined;
-    for (0..lanes(T)) |i| {
-        arr[i] = i;
-    }
-    const vek: @Vector(lanes(T), usize) = arr;
-    return vek;
+    return comptime ret: {
+        var arr: [lanes(T)]usize = undefined;
+        for (0..lanes(T)) |i| {
+            arr[i] = i;
+        }
+        break :ret arr;
+    };
 }
 
-fn nans_vek(comptime T: type, s: Stream(T), vek_i: usize) Vek(bool) {
-    const index_vec = comptime idx_vek(T);
-    const i = vek_i * lanes(T);
-    const stop = @max(@min(s.stop - i, lanes(T)), 0);
-    const start_idx = @max(@min(i - s.start, lanes(T)), 0);
-    return (index_vec >= stop) | (index_vec < start_idx);
+fn nans_vek(comptime T: type, start_idx: usize, stop: usize, vek_i: usize) @Vector(lanes(T), bool) {
+    const i: usize = vek_i * lanes(T);
+    const i_vek: @Vector(lanes(T), usize) = @splat(i);
+    const index_vec = idx_vek(T) + i_vek;
+    const start_idx_vek: @Vector(lanes(T), usize) = @splat(start_idx);
+    const stop_vek: @Vector(lanes(T), usize) = @splat(stop);
+    const lhs: @Vector(lanes(T), u1) = @bitCast(index_vec >= stop_vek);
+    const rhs: @Vector(lanes(T), u1) = @bitCast(index_vec < start_idx_vek);
+    const res: @Vector(lanes(T), bool) = @bitCast(lhs | rhs);
+    return res;
 }
 
 fn to_slice(comptime T: type, s: Stream(T)) []T {
@@ -252,23 +272,36 @@ pub fn apply(
             .Sub => a_vek - b_vek,
             .Div => a_vek / b_vek,
             .Mul => a_vek * b_vek,
-            .Mod => a_vek % b_vek,
+            .Mod => @mod(a_vek, b_vek),
             .Min => @min(a_vek, b_vek),
             .Max => @max(a_vek, b_vek),
             .Log => @log2(b_vek) / @log2(a_vek),
             .Pow => @exp2(b_vek * @log2(a_vek)),
-            .LShift => a_vek << b_vek,
-            .RShift => a_vek >> b_vek,
-            .SLShift => a_vek <<| b_vek,
-            .And => a_vek & b_vek,
-            .Or => a_vek | b_vek,
-            .Xor => a_vek ^ b_vek,
+            // .LShift => a_vek << b_vek,
+            // .RShift => a_vek >> b_vek,
+            // .SLShift => a_vek <<| b_vek,
+            .And, .Or, .Xor => blk: {
+                const a_bits: @Vector(TARGET_SIMD_BITS, u1) = @bitCast(a_vek);
+                const b_bits: @Vector(TARGET_SIMD_BITS, u1) = @bitCast(b_vek);
+                const c_bits: @Vector(TARGET_SIMD_BITS, u1) = switch (op) {
+                    .And => a_bits & b_bits,
+                    .Or => a_bits | b_bits,
+                    .Xor => a_bits ^ b_bits,
+                    else => comptime unreachable,
+                };
+                const c_vek: Vek(T) = @bitCast(c_bits);
+                break :blk c_vek;
+            },
         };
         if (mode == .Grow) {
-            const a_nans = nans_vek(T, a, i);
-            c.ptr[i] = @select(T, a_nans, b_vek, c.ptr[i]);
-            const b_nans = nans_vek(T, b, i);
-            c.ptr[i] = @select(T, b_nans, a_vek, c.ptr[i]);
+            if (a_t == .Vector) {
+                const a_nans = nans_vek(T, a.start, a.stop, i);
+                c.ptr[i] = @select(T, a_nans, b_vek, c.ptr[i]);
+            }
+            if (b_t == .Vector) {
+                const b_nans = nans_vek(T, b.start, b.stop, i);
+                c.ptr[i] = @select(T, b_nans, a_vek, c.ptr[i]);
+            }
         }
     }
 }
@@ -282,7 +315,7 @@ pub const BoolOp = enum {
     Neq,
 };
 
-fn apply_bool(
+pub fn apply_bool(
     comptime T: type,
     comptime Op: BoolOp,
     comptime mode: Apply_Mode,
@@ -336,9 +369,9 @@ fn apply_bool(
             .Neq => a_vek != b_vek,
         };
         if (mode == .Grow) {
-            const a_nans = nans_vek(T, a, i);
+            const a_nans = nans_vek(T, a.start, a.stop, i);
             c.ptr[i] = @select(T, a_nans, b_vek, c.ptr[i]);
-            const b_nans = nans_vek(T, b, i);
+            const b_nans = nans_vek(T, b.start, b.stop, i);
             c.ptr[i] = @select(T, b_nans, a_vek, c.ptr[i]);
         }
     }
@@ -377,7 +410,6 @@ pub const SimdOp3 = enum {
     Fma,
 };
 
-//TODO rewrite test w new framework
 test "basic add" {
     const num_t = f64;
 
@@ -390,13 +422,13 @@ test "basic add" {
     var streams = try make(num_t, state.allocator, 11, 4);
 
     var a_arr = [_]num_t{ 0, 1, 7, 69, 420, 666, 6969, 50, 100, 10, 20 };
-    const a_stream = slice_to_stream(num_t, &streams, &a_arr, 0);
+    const a_stream = to_stream(num_t, &streams, &a_arr, 0);
 
     var b_arr = [_]num_t{ 0, 1, 3, 96, 580, 444, 9696, 50, 100, 10, 20 };
-    const b_stream = slice_to_stream(num_t, &streams, &b_arr, 0);
+    const b_stream = to_stream(num_t, &streams, &b_arr, 0);
 
     var c_expect_arr = [_]num_t{ 0, 2, 10, 165, 1000, 1110, 16665, 100, 200, 20, 40 };
-    const c_expect_stream = slice_to_stream(num_t, &streams, &c_expect_arr, 0);
+    const c_expect_stream = to_stream(num_t, &streams, &c_expect_arr, 0);
 
     var c_actual_stream = new_stream(num_t, &streams);
     apply(num_t, .Add, .Strict, &c_actual_stream, .Vector, a_stream, .Vector, b_stream);
@@ -405,39 +437,86 @@ test "basic add" {
     try testing.expectEqualSlices(num_t, to_slice(num_t, c_expect_stream), to_slice(num_t, c_actual_stream));
 }
 
-// //TODO rewrite test w new framework
-// test "grow add" {
-//     const num_t = f64;
-//     const ninf = -std.math.inf(num_t);
-//     var a_arr = [_]Vek(num_t){ .{ 0, 1, 7, 69, 420, 666, 6969, 50 }, .{ 100, 10, 20, ninf, ninf, ninf, ninf, ninf } };
-//     const a: Stream(num_t) = .{
-//         .ptr = &a_arr,
-//         .stop = 11,
-//     };
-//     var b_arr = [_]Vek(num_t){ .{ 0, 1, 3, 96, 580, 444, 9696, 50 }, .{ 100, 10, 20, 30, ninf, ninf, ninf, ninf } };
-//     const b: Stream(num_t) = .{
-//         .ptr = &b_arr,
-//         .stop = 11,
-//     };
-//     var c_expect_arr = [_]Vek(num_t){ .{ 0, 2, 10, 165, 1000, 1110, 16665, 100 }, .{ 200, 20, 40, 30, ninf, ninf, ninf, ninf } };
-//     const c_expect: Stream(num_t) = .{
-//         .ptr = &c_expect_arr,
-//         .stop = 11,
-//     };
-//     var c_actual_arr: [2]Vek(num_t) = undefined;
-//     @memset(&c_actual_arr, @splat(ninf));
-//     var c_actual: Stream(num_t) = .{
-//         .ptr = &c_actual_arr,
-//     };
-//     apply(num_t, .Add, .Grow, &c_actual, .Vector, a, .Vector, b);
-//     print_vek(num_t, c_expect);
-//     print_vek(num_t, c_actual);
-//     try testing.expectEqualSlices(Vek(num_t), c_expect.ptr, c_actual.ptr);
-//     try testing.expectEqual(c_expect.start, c_actual.start);
-//     try testing.expectEqual(c_expect.stop, c_actual.stop);
-// }
+test "basic growing add" {
+    const num_t = f64;
+
+    var beig_buffer: [1024]u8 = undefined;
+    const state_res = init(&beig_buffer, 1024);
+    if (state_res.err != 0) {
+        unreachable;
+    }
+    const state = state_res.ok;
+    var streams = try make(num_t, state.allocator, 12, 4);
+
+    var a_arr = [_]num_t{ 0, 1, 7, 69, 420, 666, 6969, 50, 100, 10, 20 };
+    const a_stream = to_stream(num_t, &streams, &a_arr, 0);
+
+    var b_arr = [_]num_t{ 0, 1, 3, 96, 580, 444, 9696, 50, 100, 10, 20, 21 };
+    const b_stream = to_stream(num_t, &streams, &b_arr, 0);
+
+    var c_expect_arr = [_]num_t{ 0, 2, 10, 165, 1000, 1110, 16665, 100, 200, 20, 40, 21 };
+    const c_expect_stream = to_stream(num_t, &streams, &c_expect_arr, 0);
+
+    var c_actual_stream = new_stream(num_t, &streams);
+    apply(num_t, .Add, .Grow, &c_actual_stream, .Vector, a_stream, .Vector, b_stream);
+    // print_vek(num_t, c_expect_stream);
+    // print_vek(num_t, c_actual_stream);
+    try testing.expectEqualSlices(num_t, to_slice(num_t, c_expect_stream), to_slice(num_t, c_actual_stream));
+}
 
 // #region EXPORTS!!!
-pub export fn add(c: *Stream(f64), a: Stream(f64), b: Stream(f64)) void {
-    apply(f64, .Add, .Strict, c, .Vector, a, .Vector, b);
+// pub export fn add(c: *Stream(f64), a: Stream(f64), b: Stream(f64)) void {
+//     apply(f64, .Add, .Strict, c, .Vector, a, .Vector, b);
+// }
+
+comptime {
+    @setEvalBranchQuota(4096);
+    for (@typeInfo(Apply_Mode).@"enum".fields) |mode| {
+        const mode_enum: Apply_Mode = @enumFromInt(mode.value);
+        for (number_types) |t| {
+            var is_float_t = false;
+            for (float_types) |float_ts| {
+                if (t != float_ts) continue;
+                is_float_t = true;
+                break;
+            }
+            op_loop: for (@typeInfo(Simd_Op).@"enum".fields) |op| {
+                const op_enum: Simd_Op = @enumFromInt(op.value);
+                if (!is_float_t) {
+                    for (simd_float_ops) |float_op| {
+                        if (op_enum == float_op) {
+                            continue :op_loop;
+                        }
+                    }
+                }
+                generate_apply_func(t, op_enum, mode_enum, .Vector, .Vector);
+                generate_apply_func(t, op_enum, mode_enum, .Vector, .Number);
+                generate_apply_func(t, op_enum, mode_enum, .Number, .Vector);
+            }
+        }
+    }
+}
+
+fn generate_apply_func(
+    comptime T: type,
+    comptime op: Simd_Op,
+    comptime mode: Apply_Mode,
+    comptime a_t: Operand_Variant,
+    comptime b_t: Operand_Variant,
+) void {
+    var name: []const u8 = @tagName(op) ++ "_" ++ @typeName(T);
+    if (a_t == .Number) {
+        name = "num_" ++ name;
+    }
+    if (b_t == .Number) {
+        name = name ++ "_num";
+    }
+    if (mode != .Strict) {
+        name = name ++ "_" ++ @tagName(mode);
+    }
+    @export(&struct {
+        fn generated(c: *Stream(T), a: Operand(T, a_t), b: Operand(T, b_t)) callconv(.C) void {
+            apply(T, op, mode, c, a_t, a, b_t, b);
+        }
+    }.generated, .{ .name = name });
 }
